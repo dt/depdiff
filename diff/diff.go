32132857ead10d48c147299bd5912ba0a889b6bf
David Taylor <tinystatemachine@gmail.com>
2016-12-08 20:23:41 +0000

    automatially diff vs HEAD when there are changes
    otherwise fallback to diffing vs ~HEAD, or between explictly provided any tree-ish

---
+++ diff/diff.go
@@ -0,0 +1,112 @@
+package diff
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+// Versions a map of dependency to version.
+type Versions map[string]string
+
+type update struct {
+	name string
+	from string
+	to   string
+}
+
+type updates []update
+
+func (c updates) Len() int           { return len(c) }
+func (c updates) Less(i, j int) bool { return c[i].name < c[j].name }
+func (c updates) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+
+var _ sort.Interface = updates{}
+
+// Changes represents the changes between two sets of versions.
+type Changes struct {
+	updates updates
+	added   []string
+	removed []string
+}
+
+// GithubPrefixes maps non-github import path prefixe to github repos, for
+// generating comparison links.
+var GithubPrefixes = map[string]string{
+	"golang.org/x":                "github.com/golang",
+	"google.golang.org/appengine": "github.com/golang/appengine",
+	"google.golang.org/grpc":      "github.com/grpc/grpc-go",
+	"gopkg.in/inf.v0":             "github.com/go-inf/inf",
+	"honnef.co/go/":               "github.com/dominikh/go-",
+}
+
+func (u update) compareLink() string {
+	repo := u.name
+	for prefix, github := range GithubPrefixes {
+		if strings.HasPrefix(repo, prefix) {
+			repo = strings.Replace(repo, prefix, github, 1)
+		}
+	}
+	if strings.HasPrefix(repo, "github.com") {
+		return fmt.Sprintf("https://%s/compare/%s...%s", repo, u.from[:8], u.to[:8])
+	}
+	return ""
+}
+
+// Print writes a summary of added, removed and changed dependencies to stdout.
+func (c Changes) Print() {
+	if c.added != nil {
+		fmt.Println("Added:")
+		for _, i := range c.added {
+			fmt.Printf(" - %s\n", i)
+		}
+	}
+	if c.updates != nil {
+		fmt.Println("Changed:")
+		for _, i := range c.updates {
+			fmt.Printf(" - %s: %s -> %s\n", i.name, i.from[:8], i.to[:8])
+			if link := i.compareLink(); link != "" {
+				fmt.Printf("   - %s\n", link)
+			}
+		}
+	}
+	if c.removed != nil {
+		fmt.Println("Removed:")
+		for _, d := range c.removed {
+			fmt.Printf(" - %s\n", d)
+		}
+	}
+}
+
+func (c Changes) Links() {
+	for _, i := range c.updates {
+		if link := i.compareLink(); link != "" {
+			fmt.Println(link)
+		} else {
+			fmt.Fprintf(os.Stderr, "cannot get link for %s (%s -> %s)", i.name, i.from[:8], i.to[:8])
+		}
+	}
+}
+
+// Compare computes a Changes describing the differences between before and after.
+func Compare(before, after Versions) (Changes, error) {
+	d := Changes{}
+	for name, new := range after {
+		old, ok := before[name]
+		if !ok {
+			d.added = append(d.added, name)
+		} else if new != old {
+			d.updates = append(d.updates, update{name: name, from: old, to: new})
+		}
+	}
+	for name := range before {
+		if _, ok := after[name]; !ok {
+			d.removed = append(d.removed, name)
+		}
+	}
+	sort.Strings(d.added)
+	sort.Strings(d.removed)
+	sort.Sort(d.updates)
+	return d, nil
+}
--- main.go
+++ main.go
@@ -3,157 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
-	"sort"
-	"strings"
 	"syscall"
 
-	"gopkg.in/yaml.v2"
+	"github.com/dt/glide-diff-parser/diff"
+	"github.com/dt/glide-diff-parser/glide"
 )
-
-var rewrites = map[string]string{
-	"golang.org/x":                "github.com/golang",
-	"google.golang.org/appengine": "github.com/golang/appengine",
-	"google.golang.org/grpc":      "github.com/grpc/grpc-go",
-	"gopkg.in/inf.v0":             "github.com/go-inf/inf",
-	"honnef.co/go/":               "github.com/dominikh/go-",
-}
-
-// Versions a map of dependency to version.
-type Versions map[string]string
-
-type dep struct {
-	Name    string
-	Version string
-}
-
-type lockfile struct {
-	Imports []dep
-}
-
-func readFromGit(treeish string) (Versions, error) {
-	out, err := exec.Command("git", "show", fmt.Sprintf("%s:glide.lock", treeish)).Output()
-	if err != nil {
-		return nil, err
-	}
-	return parseGlildeYAML(out)
-}
-
-func readFromFs() (Versions, error) {
-	fp, err := os.Open("glide.lock")
-	if err != nil {
-		return nil, err
-	}
-	defer fp.Close()
-	out, err := ioutil.ReadAll(fp)
-	if err != nil {
-		return nil, err
-	}
-	return parseGlildeYAML(out)
-}
-
-func parseGlildeYAML(content []byte) (Versions, error) {
-	l := lockfile{}
-	err := yaml.Unmarshal(content, &l)
-	if err != nil {
-		return nil, err
-	}
-	m := make(map[string]string, len(l.Imports))
-	for _, dep := range l.Imports {
-		m[dep.Name] = dep.Version
-	}
-	return m, nil
-}
-
-type change struct {
-	name string
-	from string
-	to   string
-}
-
-type changes []change
-
-func (c changes) Len() int           { return len(c) }
-func (c changes) Less(i, j int) bool { return c[i].name < c[j].name }
-func (c changes) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
-
-var _ sort.Interface = changes{}
-
-// Diff represents the changes between two sets of versions.
-type Diff struct {
-	changed changes
-	added   []string
-	removed []string
-}
-
-func compareLink(i change) string {
-	repo := i.name
-	for prefix, github := range rewrites {
-		if strings.HasPrefix(repo, prefix) {
-			repo = strings.Replace(repo, prefix, github, 1)
-		}
-	}
-	if strings.HasPrefix(repo, "github.com") {
-		return fmt.Sprintf("https://%s/compare/%s...%s", repo, i.from[:8], i.to[:8])
-	}
-	return ""
-}
-
-func (d diff) Print() {
-	if d.added != nil {
-		fmt.Println("Added:")
-		for i := range d.added {
-			fmt.Printf(" - %s\n", i)
-		}
-	}
-	if d.changed != nil {
-		fmt.Println("Changed:")
-		for _, i := range d.changed {
-			fmt.Printf(" - %s: %s -> %s\n", i.name, i.from[:8], i.to[:8])
-			if link := compareLink(i); link != "" {
-				fmt.Printf("   - %s\n", link)
-			}
-		}
-	}
-	if d.removed != nil {
-		fmt.Println("Removed:")
-		for d := range d.removed {
-			fmt.Printf(" - %s\n", d)
-		}
-	}
-}
-
-func (d diff) Open() {
-	for _, i := range d.changed {
-		if link := compareLink(i); link != "" {
-			exec.Command("open", link).Run()
-		}
-	}
-}
-
-func getDiff(before, after map[string]string) (diff, error) {
-	d := diff{}
-	for name, new := range after {
-		old, ok := before[name]
-		if !ok {
-			d.added = append(d.added, name)
-		} else if new != old {
-			d.changed = append(d.changed, change{name: name, from: old, to: new})
-		}
-	}
-	for name := range before {
-		if _, ok := after[name]; !ok {
-			d.removed = append(d.removed, name)
-		}
-	}
-	sort.Strings(d.added)
-	sort.Strings(d.removed)
-	sort.Sort(d.changed)
-	return d, nil
-}
 
 func locallyChanged() (bool, error) {
 	err := exec.Command("git", "diff", "--quiet", "glide.lock").Run()
@@ -167,27 +23,84 @@ func locallyChanged() (bool, error) {
 }
 
 func main() {
-	open := flag.Bool("open", false, "attempt to open github in browser for each changed dependency")
+	verbose := flag.Bool("v", false, "print a detailed summary of added, removed and changed dependencies")
+	flag.Usage = func() {
+		help := `
+     usage: %s [from [to]]
 
+     Parse and summarize changes to glide.lock.
+
+     'from' and 'to' can be any git tree-ish, like 'master' or 'HEAD~3'
+
+      If 'to' is not specified, the current file contents is used.
+
+      If 'from' is not specified:
+        'HEAD' is used if 'glide.lock' has uncommited modifications, otherwise
+        'HEAD~' is used (effectively comparing 'HEAD~' to 'HEAD').
+
+      Flags:
+     `
+		fmt.Fprintf(os.Stderr, help, os.Args[0])
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
+	checkErr := func(e error) {
+		if e != nil {
+			fmt.Fprintf(os.Stderr, "%+v", e)
+			os.Exit(1)
+		}
+	}
+
 	args := flag.Args()
-	from, to := "HEAD~", "HEAD"
-	if len(args) > 0 {
-		from = args[0]
-	}
-	if len(args) > 1 {
-		to = args[1]
+
+	var from, to diff.Versions
+	var err error
+
+	if len(args) > 2 {
+		flag.Usage()
+		os.Exit(-1)
 	}
 
-	diff, err := getDiff(from, to)
-	if err != nil {
-		log.Fatal(err)
+	file := "glide.lock"
+	// The `after` in our comparison is always the current file unless we're given
+	// an explicit treeish as a second argument.
+	if len(args) == 2 {
+		fmt.Fprintf(os.Stderr, "Changes in %s between %s and %s\n", file, args[0], args[1])
+		from, err = glide.ReadFromGit(args[0])
+		checkErr(err)
+		to, err = glide.ReadFromGit(args[1])
+		checkErr(err)
+	} else {
+		to, err = glide.ReadFromFs()
+		checkErr(err)
+
+		since := "HEAD~"
+		// If we're given a `from` argument, we read deps from that tree-ish.
+		if len(args) == 1 {
+			since = args[0]
+		} else {
+			// Otherwise, we compare to HEAD~ unless there are are uncommitted changes
+			// in which case we just compare to HEAD.
+			changed, err := glide.LocalFileChanged()
+			checkErr(err)
+			if changed {
+				since = "HEAD"
+			}
+		}
+
+		fmt.Fprintf(os.Stderr, "Changes in %s since %s\n", file, since)
+		from, err = glide.ReadFromGit(since)
+		checkErr(err)
 	}
 
-	diff.Print()
+	d, err := diff.Compare(from, to)
+	checkErr(err)
 
-	if *open {
-		diff.Open()
+	if *verbose {
+		d.Print()
+	} else {
+		d.Links()
 	}
+
 }
